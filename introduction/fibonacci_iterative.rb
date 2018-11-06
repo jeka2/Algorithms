9230aa0d2cae3030409a451fe2b2df30d44dc857
Yevgeniy Ivanov <anigeka@gmail.com>
2018-11-06 09:24:09 -0600

    introduction complete

---
+++ introduction/fibonacci_iterative.rb
@@ -0,0 +1,16 @@
+class Fibonacci
+
+  def fib(n)
+    fib_0 = 0
+    fib_1 = 1
+    holder = [0,1]
+    1.upto(n) do |i|
+      temp = fib_1
+      fib_1 = fib_1 + fib_0
+      fib_0 = temp
+      holder << fib_1
+    end
+    holder
+  end
+
+end
